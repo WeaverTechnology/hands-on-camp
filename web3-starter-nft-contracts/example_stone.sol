fdd15923ec70083cac48410d288dd7b639fb40fd
Stone <yangzq12@users.noreply.github.com>
2023-10-14 19:40:17 +0800

    Update example_stone.sol

--- web3-starter-nft-contracts/example_stone.sol
+++ web3-starter-nft-contracts/example_stone.sol
@@ -1,78 +1,128 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract SimpleNFT {
-    using SafeMath for uint256;
+contract ERC721 /* is ERC165 */ {
 
-    // 代币名称
-    string public name = "SimpleNFT";
-    // 代币符号
-    string public symbol = "SNFT";
-    
-    // 当前代币总量
-    uint256 public totalSupply;
-    
-    // 每个代币ID对应的所有者
-    mapping(uint256 => address) public tokenOwner;
-    
-    // 持有者的代币数量
-    mapping(address => uint256) public balanceOf;
-    
-    // 代币所有者允许其他地址转移的代币数量
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address=>uint256) private _balance;
+    mapping(uint256=>address) private _ownerOf;
+    mapping(uint256=>address) private _tokenApproval;
+    mapping(address=>mapping(address=>bool)) private _operatorApproval;
 
-    // 代币的元数据URI
-    mapping(uint256 => string) private _tokenURIs;
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
 
-    // 事件
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    /// @notice Count all NFTs assigned to an owner
+    /// @dev NFTs assigned to the zero address are considered invalid, and this
+    ///  function throws for queries about the zero address.
+    /// @param _owner An address for whom to query the balance
+    /// @return The number of NFTs owned by `_owner`, possibly zero
 
-    modifier onlyOwnerOf(uint256 tokenId) {
-        require(tokenOwner[tokenId] == msg.sender, "Not token owner");
-        _;
+    /// @notice A descriptive name for a collection of NFTs in this contract
+    function name() external view returns (string memory _name){
+        return "Demo-NFT";
     }
 
-    function mint(address to, string memory tokenURI) external {
-        totalSupply = totalSupply.add(1);
-        uint256 newTokenId = totalSupply;
-        tokenOwner[newTokenId] = to;
-        balanceOf[to] = balanceOf[to].add(1);
-        _tokenURIs[newTokenId] = tokenURI;
-        emit Transfer(address(0), to, newTokenId);
+    /// @notice An abbreviated name for NFTs in this contract
+    function symbol() external view returns (string memory _symbol){
+        return "NFT";
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) external onlyOwnerOf(tokenId) {
-        tokenOwner[tokenId] = to;
-
-        balanceOf[from] = balanceOf[from].sub(1);
-        balanceOf[to] = balanceOf[to].add(1);
-        emit Transfer(from, to, tokenId);
+    /// @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+    /// @dev Throws if `_tokenId` is not a valid NFT. URIs are defined in RFC
+    ///  3986. The URI may point to a JSON file that conforms to the "ERC721
+    ///  Metadata JSON Schema".
+    function tokenURI(uint256 _tokenId) external view returns (string memory){
+        return "https://cyan-sheer-tarsier-951.mypinata.cloud/ipfs/QmWYcGTT3d12nuCZL3ZiCrSnz1vh6qs6V8Seg3LNgFVetf";
     }
 
-    function approve(address approved, uint256 tokenId) external onlyOwnerOf(tokenId) {
-        allowance[msg.sender][approved] = tokenId;
-        emit Approval(msg.sender, approved, tokenId);
+
+    constructor(){
+        mint(1, msg.sender);
     }
 
-    function tokenURI(uint256 tokenId) external view returns (string memory) {
-        return _tokenURIs[tokenId];
+    function mint(uint256 _tokenId, address _newOwner) public {
+        require(_ownerOf[_tokenId]!=address(0), "ERC721: already minted");
+        _ownerOf[_tokenId] = _newOwner;
+        _balance[_newOwner]++;
+        emit Transfer(address(0),  _newOwner, _tokenId);
+    }
+    
+    function balanceOf(address _owner) external view returns (uint256){
+        return _balance[_owner];
+    }
+
+    /// @notice Find the owner of an NFT
+    /// @dev NFTs assigned to zero address are considered invalid, and queries
+    ///  about them do throw.
+    /// @param _tokenId The identifier for an NFT
+    /// @return The address of the owner of the NFT
+    function ownerOf(uint256 _tokenId) external view returns (address){
+        require(_ownerOf[_tokenId]!=address(0), "ERC721: non-existing token");
+        return _ownerOf[_tokenId];
+    }
+
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(_ownerOf[_tokenId]== _from, "ERC721: token does not belong to from address");
+        require(msg.sender == _from || msg.sender == _tokenApproval[_tokenId] || _operatorApproval[_from][msg.sender] == true, "ERC721: invalid caller");
+        
+        _balance[_from] = _balance[_from] - 1;
+        _balance[_to] = _balance[_to] + 1;
+
+        _ownerOf[_tokenId] = _to;
+        _tokenApproval[_tokenId] = address(0);
+        emit Transfer(_from,  _to, _tokenId);
+    }
+
+    /// @notice Change or reaffirm the approved address for an NFT
+    /// @dev The zero address indicates there is no approved address.
+    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
+    ///  operator of the current owner.
+    /// @param _approved The new approved NFT controller
+    /// @param _tokenId The NFT to approve
+    function approve(address _approved, uint256 _tokenId) external payable {
+        require(msg.sender == _ownerOf[_tokenId], "ERC721: invalid caller");
+        _tokenApproval[_tokenId] = _approved;
+        emit Approval( msg.sender, _approved, _tokenId);
+    }
+
+    /// @notice Enable or disable approval for a third party ("operator") to manage
+    ///  all of `msg.sender`'s assets
+    /// @dev Emits the ApprovalForAll event. The contract MUST allow
+    ///  multiple operators per owner.
+    /// @param _operator Address to add to the set of authorized operators
+    /// @param _approved True if the operator is approved, false to revoke approval
+    function setApprovalForAll(address _operator, bool _approved) external {
+        _operatorApproval[msg.sender][_operator] = _approved;
+        emit ApprovalForAll( msg.sender,  _operator, _approved);
+    }
+
+    /// @notice Get the approved address for a single NFT
+    /// @dev Throws if `_tokenId` is not a valid NFT.
+    /// @param _tokenId The NFT to find the approved address for
+    /// @return The approved address for this NFT, or the zero address if there is none
+    function getApproved(uint256 _tokenId) external view returns (address) {
+        return _tokenApproval[_tokenId];
+    }
+
+    /// @notice Query if an address is an authorized operator for another address
+    /// @param _owner The address that owns the NFTs
+    /// @param _operator The address that acts on behalf of the owner
+    /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool) {
+        return _operatorApproval[_owner][_operator];
     }
 }
-
-library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "Addition overflow");
-
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a, "Subtraction overflow");
-        uint256 c = a - b;
-
-        return c;
-    }
-}
-
